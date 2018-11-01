75a04c31d5c1adb2a91d50fca988e5e97e0f0806
mudra <mudradimukesh@gmail.com>
2018-10-31 21:14:20 -0500

    Added Entities

--- pet-clinic-data/src/main/java/com/muki/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/muki/sfgpetclinic/model/Vet.java
@@ -1,10 +1,15 @@
 package com.muki.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch=FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",joinColumns = @JoinColumn(name = "vet_id"),inverseJoinColumns = @JoinColumn(name="speciality_id"))
     private Set<Speciality> specialities=new HashSet<Speciality>();
 
     public Set<Speciality> getSpecialities() {
